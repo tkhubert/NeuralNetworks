cc01f4eccdc3091929b823811b3603fd70f36262
tkhubert <tkhubert@gmail.com>
2015-04-07 22:47:17 +0100

    Initial Commit

---
+++ NeuralNetworks/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NeuralNetworks
+//
+//  Created by Thomas Hubert on 07/04/2015.
+//  Copyright (c) 2015 Thomas Hubert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
