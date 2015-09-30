7c77b709607515ade9c199fb5fb19cbadc99a738
tkhubert <tkhubert@gmail.com>
2015-09-30 21:02:26 +0100

    Adding comments on TKH TO DO

--- NeuralNetworks/main.cpp
+++ NeuralNetworks/main.cpp
@@ -8,8 +8,11 @@
 
 // TKH TO DO
 // 1. center/normalize data
-// 2. use dropout
-// 3. internalize the hyperparameter search : can we back prop?
+// 2. partition data properly
+// 3. implement other optimization method (adadelta, rmsprop...)
+// 4. implement stride for ConvLayers
+// 5. speed up ConvLayers
+// 6. internalize the hyperparameter search : can we back prop?
 
 #include "NN.h"
 #include "Data.h"
@@ -86,7 +89,7 @@ void CL()
     int    nbEpochs  = 100;
     
     float friction  = 0.9;
-    vector<float> lRV     =  {0.001, 0.005, 0.0075, 0.01};//{0.005, 0.01, 0.02, 0.05};//{0.001,0.002, 0.005, 0.01, 0.02};//{0.0075};//{0.001, 0.005, 0.0075, 0.01};//{0.005, 0.01, 0.02, 0.05, 0.08, 0.1, 0.15};
+    vector<float> lRV     =  {0.003};//0.001, 0.005, 0.0075, 0.01};//{0.005, 0.01, 0.02, 0.05};//{0.001,0.002, 0.005, 0.01, 0.02};//{0.0075};//{0.001, 0.005, 0.0075, 0.01};//{0.005, 0.01, 0.02, 0.05, 0.08, 0.1, 0.15};
     vector<float> lambdaV = {0};//{4};//{0.1, 1, 3, 5};
     
     vector<unique_ptr<CostFunc>> CFV;
@@ -102,8 +105,8 @@ void CL()
                 layers.emplace_back(make_unique<ConvLayer>    (28, 28,  1, 0, 0, RFunc));
                 layers.emplace_back(make_unique<ConvLayer>    (24, 24, 20, 5, 0, RFunc));
                 layers.emplace_back(make_unique<ConvPoolLayer>(12, 12, 20, 2, 0, IFunc));
-                //layers.emplace_back(make_unique<ConvLayer>    ( 8,  8, 40, 5, 0, RFunc));
-                //layers.emplace_back(make_unique<ConvPoolLayer>( 4,  4, 40, 2, 0, IFunc));
+                layers.emplace_back(make_unique<ConvLayer>    ( 8,  8, 40, 5, 0, RFunc));
+                layers.emplace_back(make_unique<ConvPoolLayer>( 4,  4, 40, 2, 0, IFunc));
                 layers.emplace_back(make_unique<FCLayer>      (100, 0.         , RFunc));
                 layers.emplace_back(make_unique<FCLayer>      (10 , 0.         , RFunc));
                 
